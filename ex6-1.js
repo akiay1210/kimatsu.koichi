ea05e84f9847dc6f45614e4ece1e5ff4ceea1d1f
akiay1210 <zm24374@josai.ac.jp>
2026-01-31 23:30:57 +0900

    Update ex6-1.js

--- ex6-1.js
+++ ex6-1.js
@@ -1,1 +1,13 @@
 
+
+function changePhoto618() {
+  document.getElementById("myPhoto2").src =
+    "image/S__75644956_0.jpg";
+
+  setTimeout(returnPhoto618, 3000);
+}
+
+function returnPhoto618() {
+  document.getElementById("myPhoto2").src =
+    "image/S__75644955_0.jpg";
+}
